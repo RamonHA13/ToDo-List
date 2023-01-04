aab832ef7b36c363d073a7d9bad22bbcc339d03a
RamonHA13 <raan.heam@gmail.com>
2023-01-04 15:26:32 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+import { mainGear, options, header} from "./dom.js";
+
+window.addEventListener("DOMContentLoaded", ()=>{
+
+
+
+});
+
+// mainGear.addEventListener("click", ()=>{
+//     mainGear.animate([
+//         { transform: 'rotate(360deg)' }
+//     ], {
+//         duration: 1000,
+//         direction: "reverse",
+//     })
+    
+// })
+
+
---
+++ dom.js
@@ -0,0 +1,5 @@
+const $ = id => document.getElementById(id);
+
+export const mainGear = $("mainGear");
+export const options = $("options");
+export const header = document.querySelector("header");
