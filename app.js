4b2471412c5a6e7f30587ba7dca3e8cfe1ae1139
RamonHA13 <raan.heam@gmail.com>
2023-01-09 16:05:28 -0600

    Refactor code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const toDoCardObjects = [];
-let iterator = 0
+let toDoCardObjects = [];
+
 
 bar.addEventListener("click", () => {
     wrapperAnimation(true,250);
@@ -9,25 +9,50 @@ closeOptions.addEventListener("click", () => {
     wrapperAnimation(false,250);
 })
 
-toDoCardForm.addEventListener("submit", (e) =>{
+toDoCardForm.addEventListener("submit", (e) => {
 
+    e.preventDefault();
     const titleContent = e.target[0].value;
     const textAreaContent = e.target[1].value;
+    const date = Date.now();
     
-    e.preventDefault();
-    addCard(titleContent,textAreaContent, iterator);
-    iterator++;
+    makeObject(date, titleContent, textAreaContent);
+    toDoCardSection.innerHTML = ""
+    toDoCardObjects.map((element, index) => addCard(element));
+    
 })
 
-const addCard = (title, text, i) =>{
+const makeObject = (date, title, text) => {
 
-    toDoCardObjects.push(new toDoCardComponent(title, text, "toDoCard"))
-    const arrayToDos = toDoCardObjects.map((card, index) =>{
-        card.id = index + 1;
-        return card.render()
-    })
+    const tarea = {
+        id: date,
+        titulo: title,
+        tarea: text,
+        state: false,
+        
+        get Id(){
+            return this.id;
+        }
+    }
 
-    toDoCardSection.innerHTML += arrayToDos[i]
-    console.log()
+    toDoCardObjects = [...toDoCardObjects,tarea]
+    console.log(toDoCardObjects)
+}
+const addCard = (props) => {
+    
+    const item = document.createElement("div");
+    item.classList.add("toDoCard")
+    item.innerHTML = `
+    <h2>${props.titulo}</h2>
+    <p>${props.tarea}</p>
+    <div class="buttons">
+        <button class="editar">Editar</button> 
+        <button class="eliminar">Eliminar</button>
+    </div>
+    `
+    console.log(item)
+    toDoCardSection.appendChild(item)
+    
+    console.log(toDoCardObjects[0].id)
 }
 
